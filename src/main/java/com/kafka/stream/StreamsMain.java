5adb80ee15baa0a0a7f44897e67df4966ff10a0d
Sandip Patil <sanppatil@gmail.com>
2020-06-11 00:22:41 -0400

    added changes

--- src/main/java/com/kafka/stream/StreamsMain.java
+++ src/main/java/com/kafka/stream/StreamsMain.java
@@ -18,8 +18,6 @@ public class StreamsMain {
 		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-		// Since the input topic uses Strings for both key and value, set the default
-		// Serdes to String.
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
